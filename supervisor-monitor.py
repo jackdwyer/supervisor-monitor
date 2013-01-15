f81618d232ac76d9cca752019f432361cc60666c
Jack Dwyer <jackjack.dwyer@gmail.com>
2013-01-15 16:20:28 +1100

    made the generation of clients more concise

--- supervisor-monitor.py
+++ supervisor-monitor.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python2.7
 #Jack Dwyer 12-12-2012
 
+from urlparse import urlparse
 from flask import Flask
 from flask import render_template
 import xmlrpclib
-
 from lib import SupervisorClient, utils
 
 app = Flask(__name__)
@@ -17,21 +17,22 @@ def list_all():
         supervisors.append(supervisor.get_details())
     return render_template('list.html', supervisors=supervisors)
 
+
+
 if __name__ == "__main__":
     clientDetails = utils.parse_settings("settings.conf")
     clients = []
     #Generate Supervisor clients
     for detail in clientDetails:
-        if (len(detail) == 2):
-            clients.append(SupervisorClient.SupervisorClient(detail[0], name=detail[1]))
-            continue
-        if (len(detail) == 1):
-            clients.append(SupervisorClient.SupervisorClient(detail[0],))
-            continue
-        else:
-            print "Parsing Error in Settings, please check"
-            sys.exit(1)
+        uri = urlparse(detail[0])
+        try:
+            name = detail[1]
+        except IndexError:
+            name = None
+        
+        clients.append(SupervisorClient(uri, name))
+        
+
 
     app.clients = clients
-
     app.run(host='0.0.0.0')
