46fc67df14d571f93c629a6ac06b9081d3f18a4b
Jack Dwyer <jackjack.dwyer@gmail.com>
2013-01-16 12:29:59 +1100

    add argparse for debug and port

--- supervisor-monitor.py
+++ supervisor-monitor.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2.7
 #Jack Dwyer 12-12-2012
 
+import argparse, sys
+
 from urlparse import urlparse
 from flask import Flask
 from flask import render_template
@@ -8,7 +10,6 @@ import xmlrpclib
 from lib import SupervisorClient, utils
 
 app = Flask(__name__)
-app.debug = True
 
 @app.route('/')
 def list_all():
@@ -20,6 +21,16 @@ def list_all():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', action='store', dest='port', default=5000,
+                        help='Set port to run on')
+
+    parser.add_argument('--debug', action='store_true', default=False,
+                    dest='debug',
+                    help='Use to start server in debug mode')
+
+    results = parser.parse_args()
+
     clientDetails = utils.parse_settings("settings.conf")
     clients = []
     #Generate Supervisor clients
@@ -32,7 +43,12 @@ if __name__ == "__main__":
         
         clients.append(SupervisorClient(uri, name))
         
+    try:
+        results.port = int(results.port)
+    except ValueError:
+        print "Error: must use a valid number for port"
+        sys.exit(1)
 
 
     app.clients = clients
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=results.debug, port=results.port)
