89a2a36e81741cb10e03ac70b168c667542d1cd5
Jack Dwyer <jackjack.dwyer@gmail.com>
2013-01-30 14:22:40 +1100

    fixed supervisorClient generation

--- supervisor-monitor.py
+++ supervisor-monitor.py
@@ -4,7 +4,7 @@
 import argparse, sys
 
 from urlparse import urlparse
-from flask import Flask
+from flask import Flask, abort, redirect, url_for
 from flask import render_template
 import xmlrpclib
 import utils
@@ -12,19 +12,57 @@ from client import SupervisorClient
 
 app = Flask(__name__)
 
+
+@app.route('/group/<selectedGroup>')
+def group_list(selectedGroup):
+    fullDetails = {}
+    supervisors = {}
+    fullDetails["totalRunning"] = 0
+    fullDetails["totalProcesses"] = 0
+
+    tables = 0
+    for group, value in app.clients.items():
+        if group == selectedGroup:
+            supervisors[group] = []
+            
+            for supervisor in value:
+                details = supervisor.get_details()
+                supervisors[group].append(details)
+                fullDetails["totalRunning"] += details["totalClientRunning"]
+                fullDetails["totalProcesses"] += details["totalClientProcesses"]
+                
+                tables += 1
+    
+    if tables == 0:
+        return redirect(url_for('list_all'))
+        
+    
+    
+    fullDetails["totalTables"] = tables
+    fullDetails["supervisors"] = supervisors
+    fullDetails["groups"] = app.groups
+    return render_template('list.html', details=fullDetails)  
+
+
 @app.route('/')
 def list_all():
     fullDetails = {}
-    supervisors = []
     fullDetails["totalRunning"] = 0
     fullDetails["totalProcesses"] = 0
-    for supervisor in app.clients:
-        details = supervisor.get_details()
-        fullDetails["totalRunning"] += details["totalClientRunning"]
-        fullDetails["totalProcesses"] += details["totalClientProcesses"]
-        supervisors.append(details)
+    print app.clients
+    
+    supervisors = {}
+    for group, value in app.clients.items():
+        supervisors[group] = []
+        for supervisor in value:
+            details = supervisor.get_details()
+            supervisors[group].append(details)
+            fullDetails["totalRunning"] += details["totalClientRunning"]
+            fullDetails["totalProcesses"] += details["totalClientProcesses"]
         
+    fullDetails["totalTables"] = app.totalTables
     fullDetails["supervisors"] = supervisors
+    fullDetails["groups"] = app.groups
     return render_template('list.html', details=fullDetails)
 
 
@@ -39,7 +77,8 @@ if __name__ == "__main__":
 
     results = parser.parse_args()
 
-    clients = []
+    groups = []
+    clients = {}
     #Generate Supervisor clients
     clientID = 0
     #for detail in clientDetails:
@@ -53,16 +92,44 @@ if __name__ == "__main__":
         
     
     supervisors = utils.read_yaml()
-    for k, vl in supervisors.items():
+    for key, value in supervisors.items():
+        uri = urlparse("%s%s%s%s%s" % (value["scheme"], "://", str(key), ":", str(value["port"])))
+        client = SupervisorClient(uri, clientID)
+        
+        #Add extra details if avaliable
         try:
-            clients.append(SupervisorClient(urlparse(vl["scheme"] + "://" + k + ":" + str(vl["port"])),
-                                            clientID, vl["name"], vl["description"]))
+            client.name = value["name"]
         except KeyError:
-            clients.append(SupervisorClient(urlparse(vl["scheme"] + "://" + k + ":" + str(vl["port"])),
-                                            clientID, vl["name"]))
+            pass
+        try:
+            client.group = value["group"]
+            if not value["group"] in groups:
+                groups.append(value["group"])
+                
+        except KeyError:
+            value["group"] = "default"
+            groups.append(value["group"])
+
+            
+        try:
+            client.description = value["description"]
+        except KeyError:
+            pass
+
+
+        try:
+            clients[value["group"]].append(client)
+        except KeyError:
+            clients[value["group"]] = []
+            clients[value["group"]].append(client)
+
+        #clients.append(client)
         clientID += 1
 
 
+    print clients
 
     app.clients = clients
+    app.groups = sorted(groups, key=str.lower)
+    app.totalTables = clientID
     app.run(host='0.0.0.0', debug=results.debug, port=results.port)
